83ee5e52679775ff727b850b3d6b20b33bf58a00
Akshay Patel <akshayrpatel@gmail.com>
2013-09-28 02:33:29 -0700

    Ignore node_modules

---
+++ conf/config.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+    consumer_key: process.env.CONSUMER_KEY || 'YOUR_CONSUMER_KEY',
+    consumer_secret: process.env.CONSUMER_SECRET || 'YOUR_CONSUMER_SECRET'
+};
